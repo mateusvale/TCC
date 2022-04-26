1dcbeca829e20d7afcc70eea67ce03634dfd8d5b
mateusvale <mateusvalexandre@gmail.com>
2022-04-25 23:56:16 -0300

    beggining of autocomplete on the map

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -20,11 +20,80 @@
 
 // }
 
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+// import Main from "../template/Main";
+// // // import axios from 'axios';
+
+
+// const headerProps = {
+//     icon: 'map-marker',
+//     title: 'Mapa',
+//     subtitle: 'Cadastre o ponto no mapa onde gostaria de apresentar sua propaganda.'
+// }
+
+// const containerStyle = {
+//   width: '400px',
+//   height: '400px'
+// };
+
+// const center = {
+//   lat: -22.940816,
+//   lng: -43.342566
+// };
+
+// const key = process.env.REACT_APP_API_KEY
+
+// function MyComponent() {
+//   const { isLoaded } = useJsApiLoader({
+//     id: 'google-map-script',
+//     googleMapsApiKey: key
+//   })
+
+//   const [map, setMap] = React.useState(null)
+
+//   const onLoad = React.useCallback(function callback(map) {
+//     const bounds = new window.google.maps.LatLngBounds();
+//     map.fitBounds(bounds);
+//     setMap(map)
+//   }, [])
+
+//   const onUnmount = React.useCallback(function callback(map) {
+//     setMap(null)
+//   }, [])
+
+//   return isLoaded ? (
+//     <Main {...headerProps}>
+//       <GoogleMap
+//         mapContainerStyle={containerStyle}
+//         center={center}
+//         zoom={10}
+//         onLoad={onLoad}
+//         onUnmount={onUnmount}
+//       >
+//         { /* Child components, such as markers, info windows, etc. */ }
+//         <></>
+//       </GoogleMap>
+//     </Main>
+//   ) : <></>
+// }
+
+// export default React.memo(MyComponent)
+
+
+
+
 import React, { Component } from "react";
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, Autocomplete } from '@react-google-maps/api';
 import Main from "../template/Main";
 // // import axios from 'axios';
-
 
 const headerProps = {
     icon: 'map-marker',
@@ -33,49 +102,84 @@ const headerProps = {
 }
 
 const containerStyle = {
-  width: '400px',
+  width: '1249px',
   height: '400px'
 };
 
 const center = {
-  lat: -22.940816,
-  lng: -43.342566
+  lat: -3.745,
+  lng: -38.523
 };
 
 const key = process.env.REACT_APP_API_KEY
 
-function MyComponent() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: key
-  })
+export default class Map extends Component {
 
-  const [map, setMap] = React.useState(null)
+  constructor (props) {
+    super(props)
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
+    this.autocomplete = null
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+    this.onLoad = this.onLoad.bind(this)
+    this.onPlaceChanged = this.onPlaceChanged.bind(this)
+  }
 
-  return isLoaded ? (
-    <Main {...headerProps}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </Main>
-  ) : <></>
+  onLoad (autocomplete) {
+    console.log('autocomplete: ', autocomplete)
+
+    this.autocomplete = autocomplete
+  }
+
+  onPlaceChanged () {
+    if (this.autocomplete !== null) {
+      console.log(this.autocomplete.getPlace())
+    } else {
+      console.log('Autocomplete is not loaded yet!')
+    }
+  }
+  // onLoad = marker => {
+  //   console.log('marker: ', marker)
+  // }
+
+  render() {
+    return (
+      <Main {...headerProps}>
+        <div className="">
+          <LoadScript googleMapsApiKey={key} libraries={["places"]}>
+            <GoogleMap 
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+            >
+              {/* <Marker onLoad={this.onLoad} position={center}/> */}
+              <Marker position={center}/>
+              <Autocomplete
+                  onLoad={this.onLoad}
+                  onPlaceChanged={this.onPlaceChanged}>
+                  <input
+                    type="text"
+                    placeholder="Customized your placeholder"
+                    style={{
+                      boxSizing: `border-box`,
+                      border: `1px solid transparent`,
+                      width: `240px`,
+                      height: `32px`,
+                      padding: `0 12px`,
+                      borderRadius: `3px`,
+                      boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                      fontSize: `14px`,
+                      outline: `none`,
+                      textOverflow: `ellipses`,
+                      position: "absolute",
+                      left: "50%",
+                      marginLeft: "-120px"
+                    }}
+                  />
+              </Autocomplete>
+            </GoogleMap>
+          </LoadScript>
+        </div>
+      </Main>
+    )
+  }
 }
-
-export default React.memo(MyComponent)
